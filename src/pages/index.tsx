dfac2e124c85128906253280220a4e142c1e05eb
Edwin Montealvo <edmdc@Edwins-MacBook-Pro.local>
2021-01-04 12:02:09 -0600

    Remove boilerplate code

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import Select from '../components/Select';
+
+// markup
+const IndexPage: React.FC = () => {
+  return <Select>Hello World</Select>;
+};
+
+export default IndexPage;
